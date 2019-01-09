2b4ac577db078f5e9e3a89e9548a75a5ae7bc3f5
GUILLERMO CRESPO AGUAYO <guillermo.crespo@utmac0012.local>
2019-01-09 18:58:56 +0100

    Primer Commit

---
+++ ChristmasDinner/Entities/Participants.swift
@@ -0,0 +1,11 @@
+//
+//  Participants.swift
+//  ChristmasDinner
+//
+//  Created by GUILLERMO CRESPO AGUAYO on 9/1/19.
+//  Copyright © 2019 GUILLERMO CRESPO AGUAYO. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+
